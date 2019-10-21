89efbac3d8fa881c8117d702344ace7f96c4f12a
MD. Masbahul <mesbahul.sohan@gmail.com>
2019-10-22 00:09:02 +0600

    update

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 function cooking(event){
-    setInterval(function(){  }, '#thirdinput'.val(),'#firstinput'.val(),'#secondinput'.val());
+    var div = document.getElementById('button1');
+    div.innerText ="cooking";
+    var second = document.getElementById('thirdinput1');
+    var second = $("thirdinput1").val();
+    console.log(second);
+    var bar = document.getElementById("bar")
+    var width = 0;
+    var i = 0;
+  var id =  setInterval(myTimer, second);
+function myTimer(){
+    var color =["blue","green","yellow","orange","red"];
+    if(width>=100){
+        clearInterval(id);
+        div.innerText ="Start cooking";
+    }
+    else{
+        width++;
+        bar.style.width = width+ '%';
+        bar.innerHTML= width*1+'%';
+        bar.style.backgroundColor=color[i];
+        i = (i+1) % color.length;
+    }
+}
 }
